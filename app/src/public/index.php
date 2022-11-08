62eb629c2b9a06463ca1244d70eb90bf2ee4405f
LePangolin <91209500+LePangolin@users.noreply.github.com>
2022-11-08 11:02:13 +0100

    build(docker): modification docker

---
+++ app/src/public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+session_start();
+
+$container = require __DIR__ . '/../../bootstrap.php';
+
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+$app->addBodyParsingMiddleware();
+
+$app->get('/', function ($request, $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+
+$app->run();
