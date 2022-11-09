8a4eb608b3c7b9378aba255b20d2ef3cd82574ba
LePangolin <91209500+LePangolin@users.noreply.github.com>
2022-11-09 14:11:42 +0100

    fix(user): création compte

--- app/src/Views/js/auth.js
+++ app/src/Views/js/auth.js
@@ -1,5 +1,3 @@
-const { divIcon } = require("leaflet");
-
 document.getElementById('username').addEventListener('input', function (e) {
     if (document.getElementById('username').value.length > 0 && document.getElementById('password').value.length > 0 && document.getElementById('password2').value.length > 0) {
         if(document.getElementById('password').value == document.getElementById('password2').value){
