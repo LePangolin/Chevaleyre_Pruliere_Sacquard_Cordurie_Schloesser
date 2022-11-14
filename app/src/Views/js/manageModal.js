d02dd454bb84d85df148bf53c3eb5d10f0221d6a
CordurieL <lucas.cordurie9@etu.univ-lorraine.fr>
2022-11-14 16:52:50 +0100

    Modification style modale

--- app/src/Views/js/manageModal.js
+++ app/src/Views/js/manageModal.js
@@ -1,5 +1,3 @@
-  
-
 // Get the modal
 var modal = document.querySelector(".addImage");
 
@@ -34,98 +32,91 @@ window.onclick = function (event) {
   }
 };
 
-
-
 let tabMD = document.getElementsByClassName("MD");
 
-Array.from(tabMD).forEach(element => {
+Array.from(tabMD).forEach((element) => {
   element.addEventListener("keyup", nonVideMD);
 });
 
-
-
-function nonVideMD(){
-
-
+function nonVideMD() {
   let nonVide = true;
-  Array.from(tabMD).forEach(element => {
-    if(element.value == ""){
+  Array.from(tabMD).forEach((element) => {
+    if (element.value == "") {
       nonVide = false;
     }
   });
-  
-  if(nonVide){
 
+  if (nonVide) {
     let div = document.createElement("div");
-    div.classList.add("flex-input");
+    div.classList.add("metadataDiv_metadataElement", "flex-input");
     div.innerHTML = `
       <input type="text" class="name MD">
       <input type="text" class="metadata MD">
       `;
-    
+
     document.getElementsByClassName("metadata")[0].appendChild(div);
 
     // remove event listener
-    Array.from(tabMD).forEach(element => {
+    Array.from(tabMD).forEach((element) => {
       element.removeEventListener("keyup", nonVideMD);
     });
-    
+
     // vide le tableau
     tmp = div.getElementsByClassName("MD");
 
     // merge les deux tableaux
     tabMD = [...tabMD, ...tmp];
 
-    console.log(tabMD); 
-
+    console.log(tabMD);
 
     // desactive l'input précédent si le nouveau est est cliqué
-    Array.from(tmp).forEach(element => {
-      element.addEventListener("click", function(){
-        Array.from(tabMD).forEach(element => {
-          if(element != div.getElementsByClassName("MD")[0] && element != div.getElementsByClassName("MD")[1]){
+    Array.from(tmp).forEach((element) => {
+      element.addEventListener("click", function () {
+        Array.from(tabMD).forEach((element) => {
+          if (
+            element != div.getElementsByClassName("MD")[0] &&
+            element != div.getElementsByClassName("MD")[1]
+          ) {
             element.disabled = true;
           }
         });
       });
     });
-    
-
-    
 
     // ajoute l'event listener
 
-    Array.from(tmp).forEach(element => {
+    Array.from(tmp).forEach((element) => {
       element.addEventListener("keyup", nonVideMD);
     });
-
   }
-  
 }
 
-document.getElementById("confirmMetaData").addEventListener("click", function(){
+document
+  .getElementById("confirmMetaData")
+  .addEventListener("click", function () {
+    let tab = document.getElementsByClassName("MD");
 
-  let tab = document.getElementsByClassName("MD");
+    if (tab.length == 0) {
+      return true;
+    } else {
+      Array.from(tab).forEach((element) => {
+        element.disabled = true;
+      });
 
-  if(tab.length == 0){
-    return true;
-  }else{
-
-    Array.from(tab).forEach(element => {
-      element.disabled = true;
-    });
-
-    let json = "{";
-    for(let i = 0; i < tab.length; i+=2){
-      if(tab[i].value != "" && tab[i+1].value != ""){      
-        json += '"' + tab[i].value + '":"' + tab[i+1].value + '"';
+      let json = "{";
+      for (let i = 0; i < tab.length; i += 2) {
+        if (tab[i].value != "" && tab[i + 1].value != "") {
+          json += '"' + tab[i].value + '":"' + tab[i + 1].value + '"';
+        }
+        if (
+          i + 2 < tab.length &&
+          tab[i + 2].value != "" &&
+          tab[i + 3].value != ""
+        ) {
+          json += ",";
+        }
       }
-      if(i+2 < tab.length  && tab[i+2].value != "" && tab[i+3].value != ""){
-        json += ",";
-      }
+      json += "}";
+      document.getElementsByClassName("metadataArray")[0].value = json;
     }
-    json += "}";
-    document.getElementsByClassName("metadataArray")[0].value = json;
-  }
-});
-
+  });
